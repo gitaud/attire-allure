fec1eb80e59a1359da1dc8416fb81c0dc6a4af6c
Denis Gitau <gitaud@outlook.com>
2022-06-10 16:13:52 +0300

    Remove print statements used to debug errors

--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -4,17 +4,21 @@ dotenv.config();
 const stripe = require("stripe")(process.env.STRIPE_KEY);
 
 router.post("/payment", (req, res) => {
-	stripe.charges.create({
-		source: req.body.tokenId,
-		amount: req.body.amount,
-		currency: "usd"
-	}, (stripeErr, stripeRes) => {
-		if (stripeErr) {
-			res.status(500).json(stripeErr);
-		} else {
-			res.status(200).json(stripeRes);
-		}
-	});
+	try {
+		stripe.charges.create({
+			source: req.body.tokenId,
+			amount: req.body.amount,
+			currency: "usd"
+		}, (stripeErr, stripeRes) => {
+			if (stripeErr) {
+				res.status(500).json(stripeErr);
+			} else {
+				res.status(200).json(stripeRes);
+			}
+		});
+	} catch(err) {
+		console.log(err);
+	}
 })
 
 module.exports = router;
