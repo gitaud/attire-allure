7cfde7371f5f175a07120bca57b1558a5751312b
Denis Gitau <gitaud@outlook.com>
2022-06-18 22:03:57 +0300

    Modifyy token string to get token from the browser's local storage

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const BASE_URL = 'http://localhost:5000/api/';
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxZTI5MzU3OGQ0NjQ0ZTNhYmY3YzUxYiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY1NDc3OTE0MiwiZXhwIjoxNjU1MDM4MzQyfQ.uDh64tbvr21kFOT24eU2KYKgMFAZ5VQKIVp31gCfnwA";
+let TOKEN;
+try {
+	TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
+} catch(err) {
+	TOKEN = '';
+}
 
 export const publicRequest = axios.create({
 	baseURL: BASE_URL
